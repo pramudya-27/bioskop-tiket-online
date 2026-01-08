8f0e147848f7d46dab78d10fbcc6e41c67a68133
pramudya-27 <dio2431058@itpln.ac.id>
2026-01-08 13:41:56 +0700

    add sql2 -dio

---
+++ bioskop (2).sql
@@ -0,0 +1,339 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jan 08, 2026 at 04:11 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.5.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bioskop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int NOT NULL,
+  `nama` varchar(40) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  `foto` varchar(255) NOT NULL,
+  `level` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `nama`, `password`, `foto`, `level`) VALUES
+(3, 'admin', '0192023a7bbd73250516f069df18b500', '', 'admin'),
+(4, 'manager', '1d0258c2440a8d19e716292b231e3190', '', 'manager');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dtl_pemesan`
+--
+
+CREATE TABLE `dtl_pemesan` (
+  `id_dtl_pemesan` int NOT NULL,
+  `kursi` int NOT NULL,
+  `id_tiket` varchar(11) NOT NULL,
+  `id_pemesan` varchar(11) NOT NULL,
+  `tgl_tayang` date DEFAULT NULL,
+  `id_sesi` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `dtl_pemesan`
+--
+
+INSERT INTO `dtl_pemesan` (`id_dtl_pemesan`, `kursi`, `id_tiket`, `id_pemesan`, `tgl_tayang`, `id_sesi`) VALUES
+(17, 20, '9', 'PMSN00003', '2026-01-12', 2),
+(19, 20, '9', 'PMSN00002', '2026-01-08', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `film`
+--
+
+CREATE TABLE `film` (
+  `id_film` int NOT NULL,
+  `judul` varchar(40) NOT NULL,
+  `rating` varchar(5) NOT NULL,
+  `durasi` time NOT NULL,
+  `id_jadwal` varchar(10) NOT NULL,
+  `id_tiket` varchar(50) DEFAULT NULL,
+  `sinopsis` text NOT NULL,
+  `score` int NOT NULL,
+  `rilis` year NOT NULL,
+  `gambar` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `film`
+--
+
+INSERT INTO `film` (`id_film`, `judul`, `rating`, `durasi`, `id_jadwal`, `id_tiket`, `sinopsis`, `score`, `rilis`, `gambar`) VALUES
+(8, 'Avatar 3', 'PG', '03:00:00', '11', '9', 'Ini adalah avatar', 9, '2025', 'AA1TjeXC.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jadwal`
+--
+
+CREATE TABLE `jadwal` (
+  `id_jadwal` int NOT NULL,
+  `tgl_mulai` date NOT NULL,
+  `tgl_berhenti` date NOT NULL,
+  `id_sesi` varchar(10) NOT NULL,
+  `id_ruang` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `jadwal`
+--
+
+INSERT INTO `jadwal` (`id_jadwal`, `tgl_mulai`, `tgl_berhenti`, `id_sesi`, `id_ruang`) VALUES
+(11, '2026-01-04', '2026-01-15', '1', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE `member` (
+  `id_member` int NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(35) NOT NULL,
+  `jk` varchar(10) NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `foto` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`id_member`, `nama`, `email`, `password`, `jk`, `tgl_lahir`, `foto`) VALUES
+(15, 'Dio Pramudya', 'diopramudia2@gmail.com', '340baf1c93c74ba7b12180de829bb724', 'Laki-laki', '2006-01-25', 'bahlil.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pemesan`
+--
+
+CREATE TABLE `pemesan` (
+  `id_pemesan` varchar(11) NOT NULL,
+  `id_member` varchar(11) NOT NULL,
+  `jml_tiket_pesan` int NOT NULL,
+  `total_harga` varchar(100) NOT NULL,
+  `tgl_pesan` date NOT NULL,
+  `status` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pemesan`
+--
+
+INSERT INTO `pemesan` (`id_pemesan`, `id_member`, `jml_tiket_pesan`, `total_harga`, `tgl_pesan`, `status`) VALUES
+('PMSN00001', '3', 1, '100000', '2026-01-07', 2),
+('PMSN00002', '3', 1, '100000', '2026-01-07', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ruang`
+--
+
+CREATE TABLE `ruang` (
+  `id_ruang` int NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `jm_kursi` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ruang`
+--
+
+INSERT INTO `ruang` (`id_ruang`, `nama`, `jm_kursi`) VALUES
+(1, 'A-1', 30),
+(5, 'B-1', 30),
+(6, 'B-2', 20),
+(7, 'C-1', 30);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sesi`
+--
+
+CREATE TABLE `sesi` (
+  `id_sesi` int NOT NULL,
+  `sesi` int NOT NULL,
+  `mulai` time NOT NULL,
+  `selesai` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sesi`
+--
+
+INSERT INTO `sesi` (`id_sesi`, `sesi`, `mulai`, `selesai`) VALUES
+(1, 1, '08:00:00', '10:00:00'),
+(2, 2, '10:15:00', '12:15:00'),
+(3, 3, '13:00:00', '16:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tiket`
+--
+
+CREATE TABLE `tiket` (
+  `id_tiket` int NOT NULL,
+  `harga` varchar(255) NOT NULL,
+  `stok` int NOT NULL,
+  `id_film` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tiket`
+--
+
+INSERT INTO `tiket` (`id_tiket`, `harga`, `stok`, `id_film`) VALUES
+(9, '100000', 3, '8');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `dtl_pemesan`
+--
+ALTER TABLE `dtl_pemesan`
+  ADD PRIMARY KEY (`id_dtl_pemesan`);
+
+--
+-- Indexes for table `film`
+--
+ALTER TABLE `film`
+  ADD PRIMARY KEY (`id_film`);
+
+--
+-- Indexes for table `jadwal`
+--
+ALTER TABLE `jadwal`
+  ADD PRIMARY KEY (`id_jadwal`);
+
+--
+-- Indexes for table `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`id_member`);
+
+--
+-- Indexes for table `pemesan`
+--
+ALTER TABLE `pemesan`
+  ADD PRIMARY KEY (`id_pemesan`);
+
+--
+-- Indexes for table `ruang`
+--
+ALTER TABLE `ruang`
+  ADD PRIMARY KEY (`id_ruang`);
+
+--
+-- Indexes for table `sesi`
+--
+ALTER TABLE `sesi`
+  ADD PRIMARY KEY (`id_sesi`);
+
+--
+-- Indexes for table `tiket`
+--
+ALTER TABLE `tiket`
+  ADD PRIMARY KEY (`id_tiket`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `dtl_pemesan`
+--
+ALTER TABLE `dtl_pemesan`
+  MODIFY `id_dtl_pemesan` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `film`
+--
+ALTER TABLE `film`
+  MODIFY `id_film` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `jadwal`
+--
+ALTER TABLE `jadwal`
+  MODIFY `id_jadwal` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `member`
+--
+ALTER TABLE `member`
+  MODIFY `id_member` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `ruang`
+--
+ALTER TABLE `ruang`
+  MODIFY `id_ruang` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `sesi`
+--
+ALTER TABLE `sesi`
+  MODIFY `id_sesi` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tiket`
+--
+ALTER TABLE `tiket`
+  MODIFY `id_tiket` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
