6d6413303e90f9a7efeb3672cca5e0b5b5e3e61d
ahmad2431018-Lab <ahmad2431018@itpln.ac.id>
2026-01-08 21:16:06 +0700

    Itpln_ilham

--- dashboard_tiket.php
+++ dashboard_tiket.php
@@ -10,7 +10,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Dashboard Tiket - Movies Mania</title>
+	<title>Dashboard Tiket - Itpln Movies</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	
